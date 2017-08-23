67bea8397419a296f9c55c6d5ddc2d64a8a2ea1d
HelloBetafish <bethany.pfeister@gmail.com>
2017-08-23 12:30:05 -0700

    can click on buttons and they switch between still and animated

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,8 +5,8 @@ $(document.body).on("click", ".select", function() {
 
   var apikey = "57fd7b1be3b84af394c2693c04cb788d";
   var animal = $(this).attr("data-info");
-  var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=" + apikey +
-  "&limit=5";
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=" + apikey +
+  "&limit=10";
 
   // // Performing our AJAX GET request
   $.ajax({
@@ -21,13 +21,18 @@ $(document.body).on("click", ".select", function() {
     for (var i = 0; i<results.length; i++){
 
       if (results[i].rating !== "r" && results[i].rating !== "pg-13"){
-        var gifDiv = $("<div class='item'>");
-        var imageUrl = results[i].images.fixed_height_still.url;
+        var gifDiv = $("<div class= 'item'>");
+        var imageUrlS = results[i].images.fixed_height_still.url;
+        var imageUrlA = results[i].images.fixed_height.url;
         var rating = results[i].rating;
         var p = $("<p>").text("Rating: " + rating)
         var animalImg = $("<img>");
-        animalImg.attr("src", imageUrl);
+        animalImg.attr("src", imageUrlS);
         animalImg.attr("alt","image");
+        animalImg.attr("data-still", imageUrlS);
+        animalImg.attr("data-animate", imageUrlA);
+        animalImg.attr("data-state", "still");
+        animalImg.addClass("gif");
         gifDiv.append(p);
         gifDiv.prepend(animalImg);
         $("#topic").prepend(gifDiv);
@@ -52,17 +57,29 @@ var createBtns = function(){
 createBtns();
 
 // This function handles events where a movie button is clicked
-      $("#add-Topic").on("click", function(event) {
-        event.preventDefault();
-        if($("#topic-input").val() !== ""){
-          // This line grabs the input from the textbox
-          var userInput = $("#topic-input").val().trim();
+$("#add-Topic").on("click", function(event) {
+  event.preventDefault();
+    if($("#topic-input").val() !== ""){
+      // This line grabs the input from the textbox
+      var userInput = $("#topic-input").val().trim();
 
-          // Adding movie from the textbox to our array
-          topics.push(userInput);
+      // Adding movie from the textbox to our array
+      topics.push(userInput);
 
-          // Calling renderButtons which handles the processing of our movie array
-          createBtns();
-        $("#topic-input").val("");
-        }
-      });
+      // Calling renderButtons which handles the processing of our movie array
+      createBtns();
+      $("#topic-input").val("");
+    }
+});
+
+$(document.body).on("click", ".gif", function(){
+  var state = $(this).attr("data-state");
+  if (state === "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+  }
+  else{
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state","still");
+  }
+});
