d346834407c0b9872ed3e9e04eed2604b3dfeb01
HelloBetafish <bethany.pfeister@gmail.com>
2017-08-24 10:46:05 -0700

    renamed some variables

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,47 @@
 // Initial array with topics
 var topics = ["Ariel", "Snow White","Sleeping Beauty","Princess Jasmine", "Cinderella","Mulan","Pocahontas", "Beauty and the Beast"]
 
-$(document.body).on("click", ".select", function() {
+//Function that creates buttons for array
+var createBtns = function(){
+  $("#topic-Btns").empty();
+  for (var i = 0; i < topics.length; i++){
+    var buttonGrp = $("<div class='btnGrp'>");
+    var button = $("<button>");
+    button.addClass("topicBtn");
+    button.attr("id","index-" + i)
+    button.attr("data-info",topics[i]);
+    button.text(topics[i]);
+    var deleteBtn = $("<button class='delete'>").text("x").attr("data-index", i);
+    buttonGrp.append(button);
+    buttonGrp.append(deleteBtn);
+    $("#topic-Btns").append(buttonGrp);
+  };
+}
+
+//Function to remove buttons from array when delete button clicked
+$(document.body).on("click", "button.delete", function(){
+  var currentIndex = $(this).attr("data-index");
+  topics.splice(currentIndex,1);
+  createBtns();
+});
+
+// Function to add user input to array of buttons
+$("#add-topic").on("click", function(event) {
+  event.preventDefault();
+    if($("#user-input").val() !== ""){
+      var userInput = $("#user-input").val().trim();
+      topics.push(userInput);
+      createBtns();
+      $("#user-input").val("");
+    }
+});
+
+// Pull gifs when topic buttons clicked
+$(document.body).on("click", ".topicBtn", function() {
 
   var apikey = "57fd7b1be3b84af394c2693c04cb788d";
-  var animal = $(this).attr("data-info");
-  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=" + apikey +
+  var subject = $(this).attr("data-info");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + subject + "&api_key=" + apikey +
   "&limit=10";
 
   // // Performing our AJAX GET request
@@ -15,68 +51,33 @@ $(document.body).on("click", ".select", function() {
   })
   // After the data comes back from the API
   .done(function(response){
-    $("#topic").empty();
+    $("#show-gifs").empty();
     var results = response.data;
     console.log(results);
     for (var i = 0; i<results.length; i++){
-
+      //Limit result ratings displayed
       if (results[i].rating !== "r" && results[i].rating !== "pg-13"){
-        var gifDiv = $("<div class= 'item'>");
+        var gifDiv = $("<div class= 'gDiv'>");
         var imageUrlS = results[i].images.fixed_height_still.url;
         var imageUrlA = results[i].images.fixed_height.url;
         var rating = results[i].rating;
         var p = $("<p>").text("Rating: " + rating);
-        var animalImg = $("<img>");
-        animalImg.attr("src", imageUrlS);
-        animalImg.attr("alt","image");
-        animalImg.attr("data-still", imageUrlS);
-        animalImg.attr("data-animate", imageUrlA);
-        animalImg.attr("data-state", "still");
-        animalImg.addClass("gif");
+        var topicImg = $("<img>");
+        topicImg.attr("src", imageUrlS);
+        topicImg.attr("alt","image");
+        topicImg.attr("data-still", imageUrlS);
+        topicImg.attr("data-animate", imageUrlA);
+        topicImg.attr("data-state", "still");
+        topicImg.addClass("gif");
         gifDiv.append(p);
-        gifDiv.prepend(animalImg);
-        $("#topic").prepend(gifDiv);
+        gifDiv.prepend(topicImg);
+        $("#show-gifs").prepend(gifDiv);
       }
     } 
   });
 });
 
-
-//Function that creates buttons for array
-var createBtns = function(){
-  $("#topic-Btns").empty();
-  for (var i = 0; i < topics.length; i++){
-    var buttonGrp = $("<div class='btnGrp'>");
-  	var button = $("<button>");
-  	button.addClass("select");
-    button.attr("id","index-" + i)
-  	button.attr("data-info",topics[i]);
-  	button.text(topics[i]);
-    var deleteBtn = $("<button class='delete'>").text("x").attr("data-index", i);
-    buttonGrp.append(button);
-    buttonGrp.append(deleteBtn);
-    $("#topic-Btns").append(buttonGrp);
-  };
-}
-
-createBtns();
-
-// This function handles events where a movie button is clicked
-$("#add-Topic").on("click", function(event) {
-  event.preventDefault();
-    if($("#topic-input").val() !== ""){
-      // This line grabs the input from the textbox
-      var userInput = $("#topic-input").val().trim();
-
-      // Adding movie from the textbox to our array
-      topics.push(userInput);
-
-      // Calling renderButtons which handles the processing of our movie array
-      createBtns();
-      $("#topic-input").val("");
-    }
-});
-
+// Function to change gif state between animated and still when clicked
 $(document.body).on("click", ".gif", function(){
   var state = $(this).attr("data-state");
   if (state === "still") {
@@ -89,8 +90,5 @@ $(document.body).on("click", ".gif", function(){
   }
 });
 
-$(document.body).on("click", "button.delete", function(){
-  var currentIndex = $(this).attr("data-index");
-  topics.splice(currentIndex,1);
-  createBtns();
-});
+//Initialize page with buttons from initial array
+createBtns();
